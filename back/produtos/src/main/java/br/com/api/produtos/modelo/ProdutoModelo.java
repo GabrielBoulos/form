46c27f67c4cdfc27302203c3f526d6a9dc40bdb3
GabrielBoulos <gabrielboulosss@gmail.com>
2023-07-23 15:19:04 -0300

    first commit

---
+++ back/produtos/src/main/java/br/com/api/produtos/modelo/ProdutoModelo.java
@@ -0,0 +1,21 @@
+package br.com.api.produtos.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "produtos")
+@Getter
+@Setter
+
+public class ProdutoModelo {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String nome;
+    private String marca;
+}
